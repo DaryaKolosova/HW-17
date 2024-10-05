0fe64b96be59d77253c7fbdb3156b68c5f833a99
DaryaKolosova <darya-kolosova@mail.ru>
2024-10-05 12:35:31 +0300

    HW 17 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Registration.check("Dcddddddffgbilhjol!",
+                    "hhhhhhhhh__________", "hhhhhhhhh__________");
+            System.out.println("Логин завершен!");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
